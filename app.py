e7c00dab01ffe347cf1738eb25e6c1e578434f69
AnyaBne <49951220+AnyaBne@users.noreply.github.com>
2023-12-01 20:31:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,29 @@ import streamlit as st
 st.title('Hello Everybodyy')
 st.write('Hellooooooooo.')
 
-import streamlit as st
-
-# Exemple de liste de chansons (vous pouvez remplacer cela par votre propre liste ou base de données)
+# Exemple de liste de chansons (remplacez ceci par votre propre liste ou base de données)
 songs_list = ['Song 1', 'Song 2', 'Song 3', 'Song 4', 'Song 5']
 
 # Fonction pour générer des recommandations (à implémenter selon votre logique)
-def generate_recommendations(selected_song):
+def generate_recommendations(selected_songs):
     # Ici, implémentez votre logique pour générer des recommandations
-    # Actuellement, cela renvoie simplement une liste de chansons
-    return ['Recommended Song 1', 'Recommended Song 2', 'Recommended Song 3']
+    # Pour l'instant, cela renvoie simplement une liste de chansons non sélectionnées
+    recommended_songs = [song for song in songs_list if song not in selected_songs]
+    return recommended_songs[:3]  # Retourne les 3 premières recommandations
 
 # Interface utilisateur
 st.title('Music Recommendation Engine')
 
-# Menu déroulant pour sélectionner une chanson
-selected_song = st.selectbox('Select a song you like:', songs_list)
+# Menu déroulant pour sélectionner plusieurs chansons
+selected_songs = st.multiselect('Select songs you like:', songs_list)
 
 # Bouton pour obtenir des recommandations
 if st.button('Recommend Songs'):
-    recommendations = generate_recommendations(selected_song)
-    st.write('Recommended Songs for you:')
-    for song in recommendations:
-        st.write(song)
+    if selected_songs:
+        recommendations = generate_recommendations(selected_songs)
+        st.write('Recommended Songs for you:')
+        for song in recommendations:
+            st.write(song)
+    else:
+        st.write('Please select at least one song.')
 
