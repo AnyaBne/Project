31088b8c968bf0f2718590aa46a221b4e2d55e49
AnyaBne <49951220+AnyaBne@users.noreply.github.com>
2023-12-01 21:13:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,22 +9,25 @@ def generate_recommendations(selected_songs):
     # Cette fonction est simplifiée pour l'exemple
     return [song for song in songs_list if song not in selected_songs][:3]
 
-# Fonctions pour contrôler l'affichage des pages
+# Fonction pour afficher la page d'accueil
 def show_home_page():
     st.title('Welcome to the Music Recommendation Engine')
-    user_id = st.text_input('Enter your ID:', key="user_id")
-    if st.button('Login'):
+    user_id = st.text_input('Enter your ID:', key="user_id_input")
+    if st.button('Login', key='login_button'):
         if user_id:
             st.session_state['page'] = 'recommendation'
+            st.experimental_rerun()
         else:
             st.error('Please enter a valid ID.')
 
+# Fonction pour afficher la page de recommandation
 def show_recommendation_page():
     st.title('Music Recommendation Engine')
-    if st.button('Back to Home'):
+    if st.button('Back to Home', key='back_home_button'):
         st.session_state['page'] = 'home'
-    selected_songs = st.multiselect('Select songs you like:', songs_list, key="selected_songs")
-    if st.button('Recommend Songs'):
+        st.experimental_rerun()
+    selected_songs = st.multiselect('Select songs you like:', songs_list, key="selected_songs_multiselect")
+    if st.button('Recommend Songs', key='recommend_button'):
         if selected_songs:
             recommendations = generate_recommendations(selected_songs)
             st.subheader('Recommended Songs for you:')
