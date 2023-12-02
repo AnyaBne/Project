bcc742151c37ca66f7ea9af35b827a06006a3553
AnyaBne <49951220+AnyaBne@users.noreply.github.com>
2023-12-02 12:38:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,49 @@
 import streamlit as st
-import pandas as pd
 
-# Charger le dataset
-data = pd.read_csv('song_dataset.csv')
+# Exemple de liste de chansons
+songs_list = ['Song 1', 'Song 2', 'Song 3', 'Song 4', 'Song 5']
 
-# Extraire les IDs uniques des utilisateurs
-unique_user_ids = data['user'].unique()
+# Fonction pour générer des recommandations
+def generate_recommendations(selected_songs):
+    return [song for song in songs_list if song not in selected_songs][:3]
 
-# Initialisation des états de session
-if 'current_page' not in st.session_state:
-    st.session_state.current_page = 'home'
-
-# Fonction pour afficher la page d'accueil
+# Fonctions pour contrôler l'affichage des pages
 def show_home_page():
     st.title('Welcome to the Music Recommendation Engine')
-    user_id = st.text_input('Enter your ID:', key="user_id_input")
-    if st.button('Login', key='login_button'):
-        if user_id in unique_user_ids:
-            st.session_state.current_page = 'recommendation'
+    user_id = st.text_input('Enter your ID:', key="user_id")
+    if st.button('Login'):
+        if user_id:
+            st.session_state['page'] = 'recommendation'
+            st.session_state['user_id'] = user_id  # Stocker l'ID utilisateur dans l'état de la session
         else:
-            st.error('ID doesn\'t exist. Please try again.')
+            st.error('Please enter a valid ID.')
 
-# Fonction pour afficher la page de recommandation
 def show_recommendation_page():
     st.title('Music Recommendation Engine')
-    if st.button('Back to Home', key='back_home_button'):
-        st.session_state.current_page = 'home'
+    # Affichage du message de bienvenue avec l'ID de l'utilisateur
+    st.write(f"Welcome user {st.session_state['user_id']}.")
+    
+    if st.button('Back to Home'):
+        st.session_state['page'] = 'home'
+    
+    selected_songs = st.multiselect('Select songs you like:', songs_list, key="selected_songs")
+    if st.button('Recommend Songs'):
+        if selected_songs:
+            recommendations = generate_recommendations(selected_songs)
+            st.subheader('Recommended Songs for you:')
+            for song in recommendations:
+                st.write(song)
+        else:
+            st.warning('Please select at least one song.')
+
+# Initialisation de l'état de la session
+if 'page' not in st.session_state:
+    st.session_state['page'] = 'home'
+if 'user_id' not in st.session_state:
+    st.session_state['user_id'] = ''
 
 # Affichage des pages en fonction de l'état
-if st.session_state.current_page == 'home':
+if st.session_state['page'] == 'home':
     show_home_page()
-elif st.session_state.current_page == 'recommendation':
+elif st.session_state['page'] == 'recommendation':
     show_recommendation_page()
