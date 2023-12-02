2565d1d512f9f2ce44fe4b72d9cee67adc7d7e37
AnyaBne <49951220+AnyaBne@users.noreply.github.com>
2023-12-02 12:45:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
 import streamlit as st
-import pandas as pd
-
-# Charger le dataset (assurez-vous que le chemin est correct)
-data = pd.read_csv('song_dataset.csv')
-
-# Extraire les IDs uniques des utilisateurs
-unique_user_ids = data['user'].unique()
 
 # Exemple de liste de chansons
 songs_list = ['Song 1', 'Song 2', 'Song 3', 'Song 4', 'Song 5']
@@ -14,24 +7,31 @@ songs_list = ['Song 1', 'Song 2', 'Song 3', 'Song 4', 'Song 5']
 def generate_recommendations(selected_songs):
     return [song for song in songs_list if song not in selected_songs][:3]
 
-# Fonction pour afficher la page d'accueil
+# Fonctions pour contrôler l'affichage des pages
 def show_home_page():
     st.title('Welcome to the Music Recommendation Engine')
-    st.write(f"Welcome user {st.session_state['user_id']}.")
-    user_id = st.text_input('Enter your ID:', key="user_id_input")
-    if st.button('Login', key='login_button'):
-        if user_id in unique_user_ids:
+    user_id = st.text_input('Enter your ID:', key="user_id")
+    if st.button('Login'):
+        if user_id:
             st.session_state['page'] = 'recommendation'
+            st.session_state['user_id'] = user_id
         else:
-            st.error('ID doesn\'t exist. Please try again.')
+            st.error('Please enter a valid ID.')
 
-# Fonction pour afficher la page de recommandation
 def show_recommendation_page():
     st.title('Music Recommendation Engine')
-    if st.button('Back to Home', key='back_home_button'):
+    
+    # Vérifier si l'utilisateur est défini, sinon retourner à la page d'accueil
+    if 'user_id' not in st.session_state or not st.session_state['user_id']:
         st.session_state['page'] = 'home'
-    selected_songs = st.multiselect('Select songs you like:', songs_list, key="selected_songs_multiselect")
-    if st.button('Recommend Songs', key='recommend_button'):
+        return
+
+    st.write(f"Welcome user {st.session_state['user_id']}.")
+
+    if st.button('Back to Home'):
+        st.session_state['page'] = 'home'
+    selected_songs = st.multiselect('Select songs you like:', songs_list, key="selected_songs")
+    if st.button('Recommend Songs'):
         if selected_songs:
             recommendations = generate_recommendations(selected_songs)
             st.subheader('Recommended Songs for you:')
