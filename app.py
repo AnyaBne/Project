9c68eb1e97e0198855849398bdeaa95c47c74a74
AnyaBne <49951220+AnyaBne@users.noreply.github.com>
2023-12-02 11:51:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import streamlit as st
+import pandas as pd
+
+# Charger le dataset (assurez-vous que le chemin est correct)
+data = pd.read_csv('song_dataset.csv')
+
+# Extraire les IDs uniques des utilisateurs
+unique_user_ids = data['user'].unique()
 
 # Exemple de liste de chansons
 songs_list = ['Song 1', 'Song 2', 'Song 3', 'Song 4', 'Song 5']
 
 # Fonction pour générer des recommandations
 def generate_recommendations(selected_songs):
-    # Implémentez votre logique ici
-    # Cette fonction est simplifiée pour l'exemple
     return [song for song in songs_list if song not in selected_songs][:3]
 
 # Fonction pour afficher la page d'accueil
@@ -14,18 +19,16 @@ def show_home_page():
     st.title('Welcome to the Music Recommendation Engine')
     user_id = st.text_input('Enter your ID:', key="user_id_input")
     if st.button('Login', key='login_button'):
-        if user_id:
+        if user_id in unique_user_ids:
             st.session_state['page'] = 'recommendation'
-            st.experimental_rerun()
         else:
-            st.error('Please enter a valid ID.')
+            st.error('ID doesn\'t exist. Please try again.')
 
 # Fonction pour afficher la page de recommandation
 def show_recommendation_page():
     st.title('Music Recommendation Engine')
     if st.button('Back to Home', key='back_home_button'):
         st.session_state['page'] = 'home'
-        st.experimental_rerun()
     selected_songs = st.multiselect('Select songs you like:', songs_list, key="selected_songs_multiselect")
     if st.button('Recommend Songs', key='recommend_button'):
         if selected_songs:
@@ -36,10 +39,11 @@ def show_recommendation_page():
         else:
             st.warning('Please select at least one song.')
 
-# Affichage des pages en fonction de l'état
+# Initialisation de l'état de la session
 if 'page' not in st.session_state:
     st.session_state['page'] = 'home'
 
+# Affichage des pages en fonction de l'état
 if st.session_state['page'] == 'home':
     show_home_page()
 elif st.session_state['page'] == 'recommendation':
