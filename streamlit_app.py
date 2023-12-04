04ff64179749c697dad3f323dfbcaae64ca22264
AnyaBne <49951220+AnyaBne@users.noreply.github.com>
2023-12-04 13:08:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,84 +1,62 @@
 import streamlit as st
 import pandas as pd
-import joblib
-import requests
-from io import BytesIO
+from surprise import SVD, Dataset, Reader
+from surprise.model_selection import train_test_split
 
 # Charger le dataset
-# Assurez-vous que le chemin d'accès au fichier CSV est correct
-data = pd.read_csv('song_dataset.csv')
+df = pd.read_csv('song_dataset.csv')
 
-@st.cache(allow_output_mutation=True)
-def load_model_from_gdrive(url):
-    # Modification de l'URL pour obtenir un lien de téléchargement direct
-    url = url.replace('/file/d/', '/uc?export=download&id=').replace('/view?usp=sharing', '')
-    response = requests.get(url)
-    model_stream = BytesIO(response.content)
-    model = joblib.load(model_stream)
-    return model
+# Configuration de Surprise
+reader = Reader(rating_scale=(df['play_count'].min(), df['play_count'].max()))
+data = Dataset.load_from_df(df[['user', 'song', 'play_count']], reader)
 
-# Remplacez par votre lien de téléchargement direct
-model_url = "https://drive.google.com/uc?export=download&id=1eyXrYRk2PGi8qIeDeTzYEvXvy2iZ3kYe"  
-model = load_model_from_gdrive(model_url)
+# Utilisation de SVD
+algo = SVD()
 
-# Extraire les IDs uniques des utilisateurs
-unique_user_ids = data['user'].unique()
+# Entraînement du modèle
+trainset = data.build_full_trainset()
+algo.fit(trainset)
 
-# Fonction pour obtenir des recommandations basées sur le modèle
-def get_recommendations(user_id, model, n=10):
-    listened_songs = data[data['user'] == user_id]['song'].unique()
-    
-    all_songs = data[~data['song'].isin(listened_songs)]['song'].unique()
-    
+# Fonction pour obtenir des recommandations
+def get_recommendations(user_id, n=10):
+    listened_songs = df[df['user'] == user_id]['song'].unique()
+    all_songs = df[~df['song'].isin(listened_songs)]['song'].unique()
     predictions = []
     for song in all_songs:
-        predictions.append((song, model.predict(user_id, song).est))
-    
+        predictions.append((song, algo.predict(user_id, song).est))
     predictions.sort(key=lambda x: x[1], reverse=True)
-    
     recommended_song_ids = [song for song, _ in predictions[:n]]
-    recommended_songs = data[data['song'].isin(recommended_song_ids)][['song', 'title']].drop_duplicates().head(n)
-    
+    recommended_songs = df[df['song'].isin(recommended_song_ids)][['song', 'title']].drop_duplicates().head(n)
     return recommended_songs
 
-# Fonction pour afficher la page d'accueil
+# Fonctions pour l'interface Streamlit
 def show_home_page():
     st.title('Welcome to the Music Recommendation Engine')
     user_id = st.text_input('Enter your ID:', key="user_id_input")
     if st.button('Login', key='login_button'):
-        if user_id in unique_user_ids:
+        if user_id in df['user'].unique():
             st.session_state['page'] = 'recommendation'
             st.session_state['user_id'] = user_id
         else:
             st.error('ID doesn\'t exist. Please try again.')
 
-# Fonction pour afficher la page de recommandation
-def show_recommendation_page(model):
+def show_recommendation_page():
     st.title('Music Recommendation Engine')
     st.write(f"Welcome user {st.session_state.get('user_id', '')}.")
-
     if 'user_id' in st.session_state and st.session_state['user_id']:
-        user_id = st.session_state['user_id']
-        recommended_songs = get_recommendations(user_id, model)
+        recommended_songs = get_recommendations(st.session_state['user_id'])
         st.subheader('Recommended Songs for you:')
         for index, row in recommended_songs.iterrows():
-            st.write(f"{row['title']} (Song ID: {row['song']})")
-
+            st.write(f"{row['song']} - {row['title']}")
     if st.button('Back to Home', key='back_home_button'):
         st.session_state['page'] = 'home'
 
-# Initialisation de l'état de la session
+# Affichage des pages en fonction de l'état de la session
 if 'page' not in st.session_state:
     st.session_state['page'] = 'home'
-if 'user_id' not in st.session_state:
-    st.session_state['user_id'] = ''
 
-# Affichage des pages en fonction de l'état
 if st.session_state['page'] == 'home':
     show_home_page()
 elif st.session_state['page'] == 'recommendation':
-    show_recommendation_page(model)
+    show_recommendation_page()
 
-# Ajouter cette ligne pour exécuter l'application localement
-if __name__ == "__main__":
-    st.run()
