fbb5c0f32fa555013be8c3dfdaf7294eeee0f933
AnyaBne <49951220+AnyaBne@users.noreply.github.com>
2023-12-04 13:04:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,42 +5,39 @@ import requests
 from io import BytesIO
 
 # Charger le dataset
+# Assurez-vous que le chemin d'accès au fichier CSV est correct
 data = pd.read_csv('song_dataset.csv')
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def load_model_from_gdrive(url):
+    # Modification de l'URL pour obtenir un lien de téléchargement direct
+    url = url.replace('/file/d/', '/uc?export=download&id=').replace('/view?usp=sharing', '')
     response = requests.get(url)
     model_stream = BytesIO(response.content)
     model = joblib.load(model_stream)
     return model
 
 # Remplacez par votre lien de téléchargement direct
-model_url = "https://drive.google.com/u/0/uc?id=1eyXrYRk2PGi8qIeDeTzYEvXvy2iZ3kYe&export=download"  
+model_url = "https://drive.google.com/file/d/1eyXrYRk2PGi8qIeDeTzYEvXvy2iZ3kYe/view?usp=sharing"  
 model = load_model_from_gdrive(model_url)
-
 
 # Extraire les IDs uniques des utilisateurs
 unique_user_ids = data['user'].unique()
 
 # Fonction pour obtenir des recommandations basées sur le modèle
-def get_recommendations(user_id, n=10):
-    # Récupération des chansons écoutées par l'utilisateur
-    listened_songs = df[df['user'] == user_id]['song'].unique()
+def get_recommendations(user_id, model, n=10):
+    listened_songs = data[data['user'] == user_id]['song'].unique()
     
-    # Création d'une liste de toutes les chansons que l'utilisateur n'a pas encore écoutées
-    all_songs = df[~df['song'].isin(listened_songs)]['song'].unique()
+    all_songs = data[~data['song'].isin(listened_songs)]['song'].unique()
     
-    # Prédiction des notes pour les chansons non écoutées
     predictions = []
     for song in all_songs:
-        predictions.append((song, algo.predict(user_id, song).est))
+        predictions.append((song, model.predict(user_id, song).est))
     
-    # Tri des chansons en fonction des notes prédites et sélection des 'n' meilleures
     predictions.sort(key=lambda x: x[1], reverse=True)
     
-    # Obtention des titres de chansons
     recommended_song_ids = [song for song, _ in predictions[:n]]
-    recommended_songs = df[df['song'].isin(recommended_song_ids)][['song', 'title']].drop_duplicates().head(n)
+    recommended_songs = data[data['song'].isin(recommended_song_ids)][['song', 'title']].drop_duplicates().head(n)
     
     return recommended_songs
 
@@ -51,7 +48,7 @@ def show_home_page():
     if st.button('Login', key='login_button'):
         if user_id in unique_user_ids:
             st.session_state['page'] = 'recommendation'
-            st.session_state['user_id'] = user_id  # Stocker l'ID de l'utilisateur dans l'état de la session
+            st.session_state['user_id'] = user_id
         else:
             st.error('ID doesn\'t exist. Please try again.')
 
@@ -60,13 +57,12 @@ def show_recommendation_page(model):
     st.title('Music Recommendation Engine')
     st.write(f"Welcome user {st.session_state.get('user_id', '')}.")
 
-    # Affichage des chansons recommandées
     if 'user_id' in st.session_state and st.session_state['user_id']:
         user_id = st.session_state['user_id']
         recommended_songs = get_recommendations(user_id, model)
         st.subheader('Recommended Songs for you:')
-        for song in recommended_songs:
-            st.write(song)
+        for index, row in recommended_songs.iterrows():
+            st.write(f"{row['title']} (Song ID: {row['song']})")
 
     if st.button('Back to Home', key='back_home_button'):
         st.session_state['page'] = 'home'
@@ -82,3 +78,7 @@ if st.session_state['page'] == 'home':
     show_home_page()
 elif st.session_state['page'] == 'recommendation':
     show_recommendation_page(model)
+
+# Ajouter cette ligne pour exécuter l'application localement
+if __name__ == "__main__":
+    st.run()
